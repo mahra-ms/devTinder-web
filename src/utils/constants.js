a6276a19088238fc043343477fa4ae47787c138f
Mukesh singh Mahra <mukeshsinghmahra787@gmail.com>
2026-08-06 02:56:55 +0530

    changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,2 @@
-export const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://byte-social.onrender.com"
-    : "http://localhost:3000";
+export const BASE_URL = "https://byte-social.onrender.com"   
+// https://byte-social.onrender.com
