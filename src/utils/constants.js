8d812a527384341688872fc8dc4a5da1ae08fbb7
Mukesh singh Mahra <mukeshsinghmahra787@gmail.com>
2026-08-06 02:54:33 +0530

    fixed bug

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,4 @@
-export const BASE_URL = "http://localhost:3000"
-//https://byte-social.onrender.com
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://byte-social.onrender.com"
+    : "http://localhost:3000";
